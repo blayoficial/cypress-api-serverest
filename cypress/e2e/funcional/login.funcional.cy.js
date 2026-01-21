9040f5e7af578083edd49eae893eae5531fbdba5
Saulo Gean <mainyasuo974@gmail.com>
2026-01-21 14:40:55 -0300

    Update login.funcional.cy.js

--- cypress/e2e/funcional/login.funcional.cy.js
+++ cypress/e2e/funcional/login.funcional.cy.js
@@ -25,7 +25,7 @@ describe('Testes Funcionais - Login', () => {
     })
   })
 
-  it('02 - Login com senha invalida', () => {
+  it('02 - Login com senha inválida', () => {
     cy.request({
       method: 'POST',
       url: 'https://serverest.dev/login',
